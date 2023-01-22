ade92601a99077b474da9bc4766d2bbb8d4177e2
Bruno <Bruno_camargoSC@hotmail.com>
2023-01-21 21:53:41 -0300

    corrige erro

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const from = document.querySelector("#form-habits");
 const nlwSetup = new NLWSetup(from);
 
 const data = {
-  study: ["01-01", "01-02", "01-06", "01-06"],
+  study: ["01-01", "01-02", "01-06"],
 };
 
 nlwSetup.setData(data);
