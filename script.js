df71ae6582aca5df716efbcefdc85fe69be1d0b7
Bruno <Bruno_camargoSC@hotmail.com>
2023-01-21 21:53:10 -0300

    add JS

---
+++ script.js
@@ -0,0 +1,9 @@
+const from = document.querySelector("#form-habits");
+const nlwSetup = new NLWSetup(from);
+
+const data = {
+  study: ["01-01", "01-02", "01-06", "01-06"],
+};
+
+nlwSetup.setData(data);
+nlwSetup.load();
