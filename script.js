b862903067142663f663ec7d7f1ecce733949c68
Bruno <Bruno_camargoSC@hotmail.com>
2023-01-22 21:00:47 -0300

    final do projeto ultimo commit

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const from = document.querySelector("#form-habits");
-const nlwSetup = new NLWSetup(from);
+const form = document.querySelector("#form-habits");
+const nlwSetup = new NLWSetup(form);
+const buttons = document.querySelector("header button");
 
-const data = {
-  study: ["01-01", "01-02", "01-06"],
-};
+buttons.addEventListener("click", () => {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExiste = nlwSetup.dayExists(today);
 
+  if (dayExiste) {
+    alert("Dia já incluso 🔴");
+    return;
+  }
+
+  alert("Dia incluso com sucesso ✅");
+  nlwSetup.addDay(today);
+});
+
+form.addEventListener("change", () => {
+  localStorage.setItem("nlwHanots", JSON.stringify(nlwSetup.data));
+});
+
+const data = JSON.parse(localStorage.getItem("nlwHanots")) || {};
 nlwSetup.setData(data);
 nlwSetup.load();
